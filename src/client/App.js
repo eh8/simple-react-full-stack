cbfa6af6351e76ece84fff100133b384dd4311d8
Eric Cheng <ericcheng986@gmail.com>
2020-03-13 22:50:07 -0400

    Updates

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
 import './app.css';
-import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  state = { numDays: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    this.interval = setInterval(
+      () => fetch('/api/getNumDays')
+        .then(res => res.json())
+        .then(user => this.setState({ numDays: user.numDays })),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
-    const { username } = this.state;
+    const { numDays } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div className="App">
+        <Container>
+          <div className="AppBody">
+            <p className="display-4">
+              How many days until we should return to campus?
+            </p>
+            <h1 className="text-center display-1 py-5">
+              {numDays}
+              {' '}
+              days
+            </h1>
+            <p>
+              Type in Twitch chat to make your voice heard.
+              <br />
+              <br />
+              <code>!tomorrow</code>
+              {' '}
+              to increase
+              <br />
+              <code>!yesterday</code>
+              {' '}
+              to decrease
+              <br />
+              <br />
+            </p>
+          </div>
+        </Container>
+        <div className="bg-light pt-4 footer mt-auto">
+          <Container>
+            <p className="footer">
+              Visit cmu.edu/alerts/coronavirus for more information regarding
+              the university's response to COVID-19
+            </p>
+          </Container>
+        </div>
       </div>
     );
   }
